6a4d3638367dddfa91d83c795d06611d00bb764f
ishanc2003 <98583112+ishanc2003@users.noreply.github.com>
2024-06-28 23:05:09 +0530

    Update views.py

--- hello/views.py
+++ hello/views.py
@@ -4,6 +4,9 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+def home(request):
+    return HttpResponse("Welcome to the Home Page!")
+
 def print_message(request):
     return HttpResponse("LTIMindtree-Ishan")
 
