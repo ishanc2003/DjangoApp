482d5698589dae87c4ceb02ba195d8f9c7a393d5
ishanc2003 <98583112+ishanc2003@users.noreply.github.com>
2024-06-28 23:30:07 +0530

    Update views.py

--- hello/views.py
+++ hello/views.py
@@ -5,8 +5,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Welcome to the Home Page!")
+    return HttpResponse("Welcome to the Home Page! Try http://20.244.45.96:8000/print/")
 
 def print_message(request):
-    return HttpResponse("LTIMindtree-Ishan")
+    return HttpResponse("LTIMindtree-Ishan Azure Github CI/CD Pipelin")
 
